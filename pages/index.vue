f93ffd5c7b39df52a65f80f38f69c5dd36608fc6
Oleg Mikhailov <oleegarch.com@gmail.com>
2025-07-07 08:30:50 +0400

    Adaptive fixes

--- pages/index.vue
+++ pages/index.vue
@@ -3,17 +3,23 @@ import { skills } from '@/data/skills'
 </script>
 
 <template>
-    <div class="mt-8">
+    <div class="max-sm:px-8 mt-8">
         <span>За последние 6 лет я работал над собственными мини-проектами (VK/Telegram мини-аппы/боты), которые использовались пользователями и приносили доход. Я занимался всем: от идеи до деплоя, включая клиент, сервер, поддержку, развитие и монетизацию.</span>
     </div>
-    <div class="mt-4">
+    <div class="max-sm:px-8 mt-4">
         <span>Я всё время развивался сам: писал, читал, пробовал. Сейчас хочу прокачаться в командной разработке — и уверен, что быстро адаптируюсь. Я не боюсь фидбэка, и мне интересно расти дальше, перенимая лучшие практики.</span>
     </div>
-    <div class="font-bold text-3xl mt-12">
+    <div class="max-sm:px-8 font-bold text-3xl mt-12">
         <span>Стек</span>
     </div>
-    <div class="bg-yellow-500 rounded-md h-2 w-12 mt-4"/>
-    <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 mt-8 gap-4">
+    <div class="bg-yellow-500 rounded-md h-2 w-12 mt-4 max-sm:ml-8"/>
+    <div
+        class="
+            grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3
+            mt-8 gap-4
+            max-sm:px-8 3xs:px-0
+        "
+    >
         <div
             v-for="(skill, index) in skills"
             :key="index"
